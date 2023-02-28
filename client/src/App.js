51d623ffd65a448e0529ae0d4b4776e29d2c73a2
kevintalebi <abbas.talebi1994@gmail.com>
2023-02-28 12:26:56 +0300

    new version

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from "./utils/setAuthToken";
@@ -34,6 +36,8 @@ function App() {
             <Route exact path='/dashboard' element={<Dashboard/>}/>
             <Route exact path='/create-profile' element={<CreateProfile/>}/>
             <Route exact path='/edit-profile' element={<EditProfile/>}/>
+            <Route exact path='/add-experience' element={<AddExperience/>}/>
+            <Route exact path='/add-education' element={<AddEducation/>}/>
           </Routes>
         </Fragment>
       </BrowserRouter>
